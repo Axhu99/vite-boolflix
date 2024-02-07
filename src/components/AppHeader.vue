23ff628eb92793ba555af1908e4fb095f55ff273
Axhu99 <xhuliodreni@icloud.com>
2024-02-07 16:59:44 +0100

    add AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header class="d-flex justify-content-around align-items-center">
+        <a href="#">bootflix</a>
+        <form action="">
+            <input type="text">
+            <button>SEARCH</button>
+        </form>
+    </header>
+</template>
+
+<style>
+header {
+    background-color: #000;
+    height: 80px;
+}
+
+a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    color: red;
+    text-decoration: none;
+}
+</style>
