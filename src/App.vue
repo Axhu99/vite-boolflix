e7b9b8e510067e7862c9a165f026f43beb7923cb
Axhu99 <xhuliodreni@icloud.com>
2024-02-08 23:03:26 +0100

    fix fetch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script>
+import { api } from './data/index';
 import { store } from './data/store.js';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import axios from 'axios';
+
 
 
 export default {
@@ -10,11 +13,35 @@ export default {
     store
   }),
   components: { AppHeader, AppMain },
+  methods: {
+    fetchMovies() {
+      if (!store.filter) {
+        store.movies = [];
+        return
+      }
+      const { baseUri, language, apiKey } = api;
+
+      const params = {
+        query: store.filter,
+        api_key: apiKey,
+        language
+      }
+
+      axios.get(`${baseUri}/search/movie`, { params })
+        .then((res) => {
+          //TODO map per predere solo i dati che mi servono 
+          store.movies = res.data.results
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader @onSubmitEvent="fetchMovies" />
   <AppMain />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,6 @@
 <script>
-import { api } from '../data/index';
 import { store } from '../data/store.js';
 import SearchForm from './SearchForm.vue';
-import axios from 'axios';
 
 export default {
     name: 'AppHeader',
@@ -12,37 +10,15 @@ export default {
         setTitleFilter(titleText) {
             store.filter = titleText;
         },
-        fetchMovies() {
-            if (!store.filter) {
-                store.movies = [];
-                store.copertine = [];
-                return
-            }
-            const { baseUri, language, apiKey } = api;
-
-            const params = {
-                query: store.filter,
-                api_key: apiKey,
-                language
-            }
-
-            axios.get(`${baseUri}/search/movie`, { params })
-                .then((res) => {
-                    //TODO map per predere solo i dati che mi servono 
-                    store.movies = res.data.results
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-        }
-    }
+    },
+    emits: ['onSubmitEvent']
 }
 </script>
 
 <template>
     <header class="d-flex justify-content-around align-items-center">
         <a href="#">bootflix</a>
-        <SearchForm @term-change="setTitleFilter" @submit-form="fetchMovies" placeholder="Cerca il titolo"
+        <SearchForm @term-change="setTitleFilter" @submit-form="$emit('onSubmitEvent')" placeholder="Cerca il titolo"
             button-text="CERCA" />
     </header>
 </template>
